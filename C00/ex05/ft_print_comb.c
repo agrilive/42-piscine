915afa18efaf24d4589cadae53212b4823558af1
Wei Ching Ong <56161787+agrilive@users.noreply.github.com>
2023-06-23 14:28:08 +0800

    try again

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -1,29 +1,28 @@
-/*
-Function that displays all different combinations of 3 different digits in ascending order, listed by ascending order
-*/
-
-#include <stdio.h>
+#include <unistd.h>
 
 void ft_print_comb(void) {
-    char a = '0';
-    char b = '1';
-    char c = '2';
-
-    while (a <= '2') {
-        while (b <= '3') {
-            while (c <= '4') {
-                printf("%d\n",a);
-                printf("%d\n",b);
-                printf("%d\n",c);
-            }
-            c++;
-        }
-        b++;
-    }
-    a++;
+	char num1;
+	char num2;
+	char num3;
+	num1 = '0';
+	while (num1 <= '9') {
+		num2 = num1 + 1;
+		while (num2 <= '9') {
+			num3 = num2 + 1;
+			while (num3 <= '9') {
+				write(1, &num1, 1);
+				write(1, &num2, 1);
+				write(1, &num3, 1);
+				if (!(num1 == '7' && num2 == '8' && num3 == '9'))
+					write(1, ", ", 2);
+				num3++;
+			}
+			num2++;
+		}
+		num1++;
+	}
 }
 
 int main() {
     ft_print_comb();
-    return 0;
 }
---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+void ft_print_comb2(void) {
+    int num11 = '0';
+    int num12 = '0';
+    while (num11 <= 9) {
+        
+    }
+}
